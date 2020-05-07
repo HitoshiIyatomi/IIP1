97dc14b4b7dedba4bec01f673c464ab9ad7d6c2c
Hitoshi Iyatomi <hitoshi.iyatomi@gmail.com>
2020-05-07 15:24:27 +0900

    first two-layer network

---
+++ 02_firstNN/mnist.py
@@ -0,0 +1,132 @@
+# mnist.py
+# fist time: download mnist dataset (containing four files) 
+#   and save them to local as [mnist.pkl]
+# [if mnist.pkl exists]: load this file, store data into dataset[] array
+try:
+    import urllib.request
+except ImportError:
+    raise ImportError('You should use Python 3.x')
+import os.path
+import gzip
+import pickle
+import os
+import numpy as np
+
+
+url_base = 'http://yann.lecun.com/exdb/mnist/'
+key_file = {
+    'train_img':'train-images-idx3-ubyte.gz',
+    'train_label':'train-labels-idx1-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
+    'test_label':'t10k-labels-idx1-ubyte.gz'
+}
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__))
+# __file__ is the name of the involed file [mnist.py]
+save_file = dataset_dir + "/mnist.pkl"
+
+train_num = 60000
+test_num = 10000
+img_dim = (1, 28, 28)
+img_size = 784
+
+
+def _download(file_name):
+    file_path = dataset_dir + "/" + file_name
+
+    if os.path.exists(file_path):
+        return
+
+    print("Downloading " + file_name + " ... ")
+    urllib.request.urlretrieve(url_base + file_name, file_path)
+    print("Done")
+
+def download_mnist():
+    for v in key_file.values():
+       _download(v)
+
+def _load_label(file_name):
+    file_path = dataset_dir + "/" + file_name
+
+    print("Converting " + file_name + " to NumPy Array ...")
+    with gzip.open(file_path, 'rb') as f:
+            labels = np.frombuffer(f.read(), np.uint8, offset=8)
+    print("Done: labels.shape= ", labels.shape)
+
+    return labels
+
+def _load_img(file_name):
+    file_path = dataset_dir + "/" + file_name
+
+    print("Converting " + file_name + " to NumPy Array ...")
+    with gzip.open(file_path, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+    data = data.reshape(-1, img_size)
+    print("Done: data.shape= ",data.shape)
+
+    return data
+
+def _convert_numpy():
+    dataset = {}
+    dataset['train_img'] =  _load_img(key_file['train_img'])
+    dataset['train_label'] = _load_label(key_file['train_label'])
+    dataset['test_img'] = _load_img(key_file['test_img'])
+    dataset['test_label'] = _load_label(key_file['test_label'])
+
+    return dataset
+
+def init_mnist():
+    download_mnist()
+    dataset = _convert_numpy()
+    print("Creating pickle file ...")
+    with open(save_file, 'wb') as f:
+        pickle.dump(dataset, f, -1)
+    print("***** saved MNIST dataset in ",save_file,"*****")
+
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 10))
+    for idx, row in enumerate(T):
+        row[X[idx]] = 1
+
+    return T
+
+
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    """load MNIST dataset
+    Parameters
+    ----------
+    normalize : normalize pixel value into [0,1]
+    one_hot_label :
+        if one_hot_label is True  return label as one-hot array
+        one-hot array:  (ex) [0,0,1,0,0,0,0,0,0,0]
+    flatten : if enable, image is converted into one-dim array
+    Returns
+    -------
+    (training image, training label), (test image, test label)
+    """
+    if not os.path.exists(save_file):
+        init_mnist()
+
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+
+    if not flatten:
+         for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
+
+# check whether program is involed from command prompt
+# if true: run init_mnist(), 
+#    otherwise (ex: "import mnist" from other code, do nothing)
+if __name__ == '__main__': 
+    init_mnist()
---
+++ 02_firstNN/train_neuralnet.py
@@ -0,0 +1,63 @@
+# train_neuralnet.py
+# training the first neural network
+# you can change
+#
+import sys, os
+sys.path.append(os.pardir)  # setting for including parent directly
+import numpy as np
+import matplotlib.pyplot as plt
+from mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+# read data
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 10000   # set the number of training iteration
+train_size = x_train.shape[0]
+batch_size = 100    # set the batch size
+learning_rate = 0.1 # set the learning rate at gradient descent
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    # calculate gradients (choose either one - usually take the latter)
+    ####################################################################
+    grad = network.numerical_gradient(x_batch, t_batch)
+    #grad = network.gradient(x_batch, t_batch)
+    ####################################################################
+    
+    # update weights
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        #print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+        print("train acc, test acc | {:.4f} {:.4f}".format(train_acc, test_acc))
+
+# draw graph
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
